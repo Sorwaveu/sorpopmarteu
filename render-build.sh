1ff7e245707c93af4b4154e5c7376dd18ce9008c
Sorwaveu <keksorwave@gmail.com>
2025-06-24 19:15:16 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+# Aktualizacja npm do najnowszej wersji
+npm install -g npm@latest
+
+# Instalacja zależności Pythona
 pip install -r requirements.txt
+
+# Instalacja przeglądarek dla Playwright (wraz z zależnościami systemowymi)
 playwright install --with-deps
